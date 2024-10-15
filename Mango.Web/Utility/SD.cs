1b6ef15bb73fcbcce3078fe5c98b93154ff0d5e4
LamMN1012 <01274757673lam@gmail.com>
2024-10-16 01:10:33 +0700

    Add project files.

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public static String CouponAPIBase {  get; set; }
+        public enum ApiType
+        {
+            GET,
+            POST,
+            DELETE,
+            PUT
+        }
+    }
+}
