ebfa6cfa423588c9de6cc1203ba59080f9547774
LamMN1012 <01274757673lam@gmail.com>
2025-02-14 01:05:31 +0700

    Auth API

---
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,111 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager
+            , RoleManager<IdentityRole> roleManager, IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _db = db;
+            _jwtTokenGenerator = jwtTokenGenerator;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<bool> AssignRole(string email, string roleName)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
+            if (user != null)
+            {
+                if (!_roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+                {
+                    // Create role if it does not exist
+                    _roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
+                }
+                await _userManager.AddToRoleAsync(user, roleName);
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            var user = _db.ApplicationUsers.First(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
+
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+            if (user == null || isValid == false)
+            {
+                return new LoginResponseDto() { User = null, Token = "" };
+            }
+
+            //if user was found, generate JWT token
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            UserDto userDto = new()
+            {
+                Name = user.Name,
+                ID = user.Id,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            LoginResponseDto loginResponseDto = new LoginResponseDto()
+            {
+                User = userDto,
+                Token = token
+            };
+
+            return loginResponseDto;
+        }
+
+        public async Task<string> Register(RegisterationRequestDto registerationRequestDto)
+        {
+            ApplicationUser user = new()
+            {
+                UserName = registerationRequestDto.Email,
+                Email = registerationRequestDto.Email,
+                NormalizedEmail = registerationRequestDto.Email.ToUpper(),
+                Name = registerationRequestDto.Name,
+                PhoneNumber = registerationRequestDto.PhoneNumber
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registerationRequestDto.Password);
+                if (result.Succeeded)
+                {
+                    var userToReturn = _db.ApplicationUsers.First(u => u.UserName == registerationRequestDto.Email);
+
+                    UserDto userDto = new()
+                    {
+                        Name = userToReturn.Name,
+                        ID = userToReturn.Id,
+                        Email = userToReturn.Email,
+                        PhoneNumber = userToReturn.PhoneNumber
+                    };
+
+                    return "";
+                }
+                else
+                {
+                    return result.Errors.FirstOrDefault().Description;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return "Error Encounterd ";
+        }
+    }
+}
--- Mango.Services.CouponAPI/Migrations/20250208065418_Initial.Designer.cs
+++ Mango.Services.CouponAPI/Migrations/20250208065418_Initial.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mango.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20240903095028_SeedCouponTables")]
-    partial class SeedCouponTables
+    [Migration("20250208065418_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
